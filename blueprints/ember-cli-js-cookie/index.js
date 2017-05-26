18aca892dacde89c7717d1f1cd4f9295c116d53d
Jack Rowlingson <jack.rowlingson@gmail.com>
2017-05-26 13:29:02 -0400

    use npm js-cookie package, remove bower dep

--- blueprints/ember-cli-js-cookie/index.js
+++ blueprints/ember-cli-js-cookie/index.js
@@ -1,7 +1,5 @@
 module.exports = {
   normalizeEntityName: function() {},
 
-  afterInstall: function() {
-    return this.addBowerPackageToProject('js-cookie', '^2.0.4');
-  }
+  afterInstall: function() {}
 };
--- index.js
+++ index.js
@@ -2,14 +2,21 @@
 'use strict';
 
 var path = require('path');
+var Funnel = require('broccoli-funnel');
+var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-cli-js-cookie',
 
   included: function(app) {
     this._super.included.apply(this, arguments);
+    this.import('vendor/js.cookie.js');
+  },
 
-    var jsCookiePath = path.join(app.bowerDirectory, 'js-cookie');
-    this.app.import(path.join(jsCookiePath, 'src',  'js.cookie.js'));
+  treeForVendor: function(vendorTree) {
+    var momentTree = new Funnel(path.dirname(require.resolve('js-cookie')), {
+      files: ['js.cookie.js'],
+    });
+    return new MergeTrees([vendorTree, momentTree]);
   }
 };
